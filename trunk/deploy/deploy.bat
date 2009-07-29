048f52f15ba638af556376792f5de495ff23fb5a
Zeke Kaufman <zeke_kaufman@yahoo.com>
2009-07-29 17:41:44 +0000

    - directory maintenance.
    git-svn-id: https://zoomwhee.mgh.harvard.edu/svn_devel/OpenMIMS/branches/2009-07-29@292 ea379df5-90a0-4b1b-ac45-2d7255ffb434

--- trunk/deploy/deploy.bat
+++
@@ -1,3 +0,0 @@
-xcopy ..\build\classes . /S /Y
-jar -cvfM Open_MIMS.jar com org *.class plugins.config
-xcopy Open_MIMS.jar "\Program Files\ImageJ\plugins" /Y
--- trunk/src/SVM/libsvm/svm_node.java
+++
@@ -1,6 +0,0 @@
-package SVM.libsvm;
-public class svm_node implements java.io.Serializable
-{
-	public int index;
-	public double value;
-}
