ff34b4861216e33c8a0e07af85315131dad4cb3e
taylorwrt <taylorwrt@10.0.1.2>
2017-07-03 15:25:25 -0400

    Updated JavaDocs.

--- src/main/java/NelderMead/Function.java
+++ src/main/java/NelderMead/Function.java
@@ -6,10 +6,15 @@ package NelderMead;
 
 /**
  * A function from a vector to a scalar.
+ * 
  */
 public interface Function {
 
-    /** evaluate the function at p*/
+    /** evaluate the function at p
+     * @param p a <code>Point</code> reference. 
+     * @return at at
+     * @return getTimesCalled at
+     */
     public double at(Point p);
     public int getTimesCalled();
 
